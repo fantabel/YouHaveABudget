9a78acb8b7d822c3cb1d41241ac7bc8da2de93e0
Jimmy St-Hilaire <jimmy.sthilaire@gmail.com>
2017-08-30 15:19:14 -0400

    Transaction coded

--- YouHaveABudgetModel/src/main/java/com/fantabel/yhab/model/entity/Transaction.java
+++ YouHaveABudgetModel/src/main/java/com/fantabel/yhab/model/entity/Transaction.java
@@ -1,5 +1,7 @@
 package com.fantabel.yhab.model.entity;
 
+import com.fantabel.yhab.model.util.IdUtil;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,7 +15,30 @@ public class Transaction implements Serializable {
     private String note;
     private Category category;
     private Boolean cleared;
-
+    
+    public Transaction() {
+        this(IdUtil.getNextPositiveLong(),
+             new Payee(),
+             LocalDateTime.now(),
+             BigDecimal.TEN,
+             "Note " + IdUtil.getPositiveInteger("Note"),
+             new Category(),
+             false);
+    }
+    
+    public Transaction(Long id, Payee payee, LocalDateTime date,
+                       BigDecimal amount, String note, Category category, 
+                       Boolean cleared) {
+        this.id = id;
+        this.payee = payee;
+        this.date = date;
+        this.amount = amount;
+        this.note = note;
+        this.category = category;
+        this.cleared = cleared;
+        
+    }
+    
     public Long getId() {
         return id;
     }
@@ -22,6 +47,71 @@ public class Transaction implements Serializable {
         this.id = id;
     }
 
+    public void setPayee(Payee payee) {
+        this.payee = payee;
+    }
+
+    public Payee getPayee() {
+        return payee;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCleared(Boolean cleared) {
+        this.cleared = cleared;
+    }
+
+    public Boolean getCleared() {
+        return cleared;
+    }
     
-    
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(id);
+        sb.append('\t');
+        sb.append(payee);
+        sb.append('\t');
+        sb.append(date);
+        sb.append('\t');
+        sb.append(amount);
+        sb.append('\t');
+        sb.append(note);
+        sb.append('\t');
+        sb.append(category.getName());
+        sb.append('\t');
+        sb.append(cleared);
+        
+        return sb.toString();
+    }
 }
---
+++ YouHaveABudgetModel/src/test/java/com/fantabel/yhab/tests/entity/TransactionTest.java
@@ -0,0 +1,135 @@
+package com.fantabel.yhab.tests.entity;
+
+import com.fantabel.yhab.model.entity.Transaction;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TransactionTest {
+    public TransactionTest() {
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#getId()
+     */
+    @Test
+    public void testGetId() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#setId(Long)
+     */
+    @Test
+    public void testSetId() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#setPayee(com.fantabel.yhab.model.entity.Payee)
+     */
+    @Test
+    public void testSetPayee() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#getPayee()
+     */
+    @Test
+    public void testGetPayee() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#setDate(java.time.LocalDateTime)
+     */
+    @Test
+    public void testSetDate() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#getDate()
+     */
+    @Test
+    public void testGetDate() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#setAmount(java.math.BigDecimal)
+     */
+    @Test
+    public void testSetAmount() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#getAmount()
+     */
+    @Test
+    public void testGetAmount() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#setNote(String)
+     */
+    @Test
+    public void testSetNote() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#getNote()
+     */
+    @Test
+    public void testGetNote() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#setCategory(com.fantabel.yhab.model.entity.Category)
+     */
+    @Test
+    public void testSetCategory() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#getCategory()
+     */
+    @Test
+    public void testGetCategory() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#setCleared(Boolean)
+     */
+    @Test
+    public void testSetCleared() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#getCleared()
+     */
+    @Test
+    public void testGetCleared() {
+        fail("Unimplemented");
+    }
+
+    /**
+     * @see com.fantabel.yhab.model.entity.Transaction#toString()
+     */
+    @Test
+    public void testToString() {
+        System.out.println(new Transaction());
+        System.out.println(new Transaction());
+        System.out.println(new Transaction());
+        System.out.println(new Transaction());
+        System.out.println(new Transaction());
+    }
+}
